4c17082c16069c09f674252c51c57c39fb56ff73
GOT-youze <l2826427290@foxmail.com>
2026-04-07 15:13:54 +0800

    [Fix]: update launchsim argument forwarding

--- cyberdog_gazebo/script/launchsim.py
+++ cyberdog_gazebo/script/launchsim.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (c) 2023-2023 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
 
@@ -15,15 +15,50 @@
 # limitations under the License.
 
 import os
+import shlex
 import sys
 import time
 
-def launchsim():
+VISUAL_ARG_NAMES = {
+    'use_sim_time',
+    'hang_robot',
+    'use_lidar',
+    'use_realsense_camera',
+    'use_stereo_camera',
+    'use_ai_camera',
+    'rname',
+}
 
-    os.system('gnome-terminal -t "cyberdog_gazebo" -e "bash ./src/cyberdog_simulator/cyberdog_gazebo/script/launchgazebo.sh"')
+
+def _quote_cmd(script_path, extra_args):
+    cmd = f'bash {script_path}'
+    if extra_args:
+        cmd += ' ' + ' '.join(shlex.quote(arg) for arg in extra_args)
+    return cmd
+
+
+def _select_visual_args(extra_args):
+    visual_args = []
+    for arg in extra_args:
+        if ':=' not in arg:
+            continue
+        name, _ = arg.split(':=', 1)
+        if name in VISUAL_ARG_NAMES:
+            visual_args.append(arg)
+    return visual_args
+
+
+def launchsim(extra_args):
+    gazebo_cmd = _quote_cmd('./src/cyberdog_simulator/cyberdog_gazebo/script/launchgazebo.sh', extra_args)
+    visual_cmd = _quote_cmd(
+        './src/cyberdog_simulator/cyberdog_gazebo/script/launchvisual.sh',
+        _select_visual_args(extra_args))
+
+    os.system(f'NO_AT_BRIDGE=1 gnome-terminal --title="cyberdog_gazebo" -- bash -c "{gazebo_cmd}"')
     time.sleep(5)
-    os.system('gnome-terminal -t "cyberdog_viusal" -e "bash ./src/cyberdog_simulator/cyberdog_gazebo/script/launchvisual.sh"')
-    os.system('gnome-terminal -t "cyberdog_control" -e "bash ./src/cyberdog_simulator/cyberdog_gazebo/script/launchcontrol.sh"')
+    os.system(f'NO_AT_BRIDGE=1 gnome-terminal --title="cyberdog_viusal" -- bash -c "{visual_cmd}"')
+    os.system('NO_AT_BRIDGE=1 gnome-terminal --title="cyberdog_control" -- bash -c "bash ./src/cyberdog_simulator/cyberdog_gazebo/script/launchcontrol.sh"')
 
-if __name__=="__main__":
-    launchsim()
+
+if __name__ == "__main__":
+    launchsim(sys.argv[1:])
